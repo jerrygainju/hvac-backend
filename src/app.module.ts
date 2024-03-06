6008361f99e6909cf629df52db87fc9899efd384
unknown <ASUS@SG-LAP-12.sgdesign.com>
2024-03-06 11:45:19 +0545

    env setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SignupModule } from './Signup/signup.module';
 // import { userSchema } from './User/schema/register.schema';
+require('dotenv').config();
 
 @Module({
-  imports: [  MongooseModule.forRoot('mongodb://localhost:27017',{dbName: 'hvac-calculation'}),
+  imports: [  MongooseModule.forRoot(process.env.Database,{dbName: 'hvac-calculation'}),
   SignupModule
 ],
   controllers: [AppController],
